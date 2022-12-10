333e013aad5cabce7d29d3db174921cdd74f5e9b
Mark <mark@wearesandpit.com>
2022-12-10 12:36:04 +1030

    load images to canvas

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ let config = {
 	type: Phaser.AUTO,
 	width: 640,
 	height: 640,
+	backgroundColor: '#4488aa',
 	scene: {
 		preload,
 		create,
@@ -17,8 +18,27 @@ let config = {
 const game = new Phaser.Game(config);
 
 // load asset files for our game
-function preload() {}
+function preload() {
+	this.load.image('backboard', 'assets/images/backboard.png');
+	this.load.image('net', 'assets/images/backboard_net.png');
+	this.load.image('basketball', 'assets/images/basketball.png');
 
-function create() {}
+	this.load.audio('bounce', 'assets/audio/bounce_sound.mp3');
+	this.load.audio('whoosh', 'assets/audio/whoosh.mp3');
+}
+
+function create() {
+	const canvasWidth = this.sys.game.config.width;
+	const canvasHeight = this.sys.game.config.height;
+
+	this.add.sprite(0, 0, 'backboard').setPosition(canvasWidth / 2, 150);
+
+	this.net = this.add
+		.sprite(0, 0, 'net')
+		.setPosition(canvasWidth / 2, 203)
+		.setDepth(1);
+
+	this.ball = this.add.sprite(canvasWidth / 2, 500, 'basketball', 0).setScale(0.6);
+}
 
 function update() {}
